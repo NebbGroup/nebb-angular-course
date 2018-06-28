841e47edb68c045c828f61e0be23c1a29efe6d15
Miroslav Janeski <mja@nebb.net>
2018-06-28 15:44:41 +0200

    clean up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-  { path: 'factories', loadChildren: './factories/factories.module#FactoriesModule'},
+  { path: 'factories', loadChildren: './factories/factories.module#FactoriesModule' },
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
